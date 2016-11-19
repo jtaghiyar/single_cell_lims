9037c2efce45ecc38b064c5b3c09d3630cfcf4d2
jtaghiyar <jtaghiyar@bccrc.ca>
2016-11-18 16:19:39 -0800

    minor updates.

--- apps/khayyam/utils.py
+++ apps/khayyam/utils.py
@@ -80,48 +80,6 @@ class Runner(object):
         return proc.returncode
 
 
-class FileHandler(object):
-
-    """
-    Handles all the file/dir operations.
-    """
-
-    def __init__(self):
-        """initialize."""
-        self.wd_root = settings.WORKING_DIR_ROOT
-        self.rd_root = settings.RESULTS_DIR_ROOT
-
-    def make_tmp_wd(self, dir_name):
-        """make a temporary working dir."""
-        print "making temporary working directory ..."
-        path = os.path.join(self.wd_root, dir_name)
-        os.makedirs(path)
-
-    def make_tmp_rd(self, dir_name):
-        """make a temporary results dir."""
-        pass
-
-    def copy_wf(self, wf_name, wf_version, dir_name):
-        """copy the workflow in the given directory."""
-        pass
-
-    def rsync(self, spath, dpath):
-        """rcync from source to destination."""
-        pass
-
-    def rmdir(self, dirpath):
-        """remove directory."""
-        pass
-
-    def clone(self, repo, dpath):
-        """clone the given repository in the destination path."""
-        pass
-
-    def create_virtenv(self, reqs):
-        """create a python virtualenv usgin the given requirements."""
-        pass
-
-
 class Kronos(object):
     
     """
