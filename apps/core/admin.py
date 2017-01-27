4144d2d1303082a1311f2acda4dbeb0472723442
jtaghiyar <jtaghiyar@bccrc.ca>
2017-01-27 12:08:10 -0800

    removed SublibraryInformationInline from core.admin to make the loading time much faster.

--- apps/core/admin.py
+++ apps/core/admin.py
@@ -59,7 +59,7 @@ class LibraryAdmin(SimpleHistoryAdmin, admin.ModelAdmin):
       ),
     ]
     inlines = [
-      SublibraryInformationInline,
+      # SublibraryInformationInline,
       LibrarySampleDetailInline,
       LibraryConstructionInformationInline,
       LibraryQuantificationAndStorageInline
